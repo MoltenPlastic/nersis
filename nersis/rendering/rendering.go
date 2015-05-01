2ce1fcdd16070492eddb10ec46f0f2fcf2bffe53
Adrian Pistol <adinator1999@gmail.com>
2015-05-01 18:02:01 +0200

    More rendering placeholder stuff

--- nersis/rendering/rendering.go
+++ nersis/rendering/rendering.go
@@ -1,3 +1,12 @@
 package rendering
 
-// Should provide universal calls for all graphics systems we support
+import "github.com/go-gl/gl/v3.2-core/gl"
+
+// Should provide universal calls for all graphics systems we support, like OGL, SDL, etc..
+// Generally focusing on OGL is a good idea. Using stuff from https://github.com/go-gl is probably the best idea.
+
+func Init() {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+}
