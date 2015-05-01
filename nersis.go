106acd3c203a684d7006477b61e19bd2b3cf32bd
Adrian Pistol <adinator1999@gmail.com>
2015-05-01 17:18:12 +0200

    Epoch

---
+++ nersis.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"./nersis"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Nersis shall murder you.")
+	fmt.Print("Nersis sais: ")
+	nersis.Hello()
+}
---
+++ nersis/nersis.go
@@ -0,0 +1,7 @@
+package nersis
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello")
+}
